cc385cd802f674b7bb5d482bb8a215df2042f8fd
vqanar <anmol.arora@tuni.fi>
2022-09-07 13:02:31 +0300

    ducks basic app

---
+++ src/main/java/com/ar21anmolarora/app/Duck.java
@@ -0,0 +1,16 @@
+package com.ar21anmolarora.app;
+
+public class Duck {
+
+    public void quack(){
+        System.out.println("Ducks Quack");
+    }
+    
+    public void swim(){
+        System.out.println("Ducks Swim");
+    }
+
+    public void display(){
+        System.out.println("Show ducks");
+    }
+}
